baf8c31c6092b4a8a3b2e3445b50fbfd8a36f01e
Coleman <powerofpickle@gmail.com>
2018-07-11 23:39:41 -0500

    fixed eclipse issues

--- src/xX_look/im/a/package_Xx/PokerAI.java
+++ src/xX_look/im/a/package_Xx/PokerAI.java
@@ -2,7 +2,7 @@ package xX_look.im.a.package_Xx;
 
 public class PokerAI{
 
-	 public static void main(String[] args){
+	public static void main(String[] args){
 
         System.out.println("Hello world!");
 
