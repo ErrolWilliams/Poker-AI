93ea4c8d99184b0b0831b4a8adf47f70601b4980
ErrolWilliams <errolw@US-ERROLW-E7470.us.trendnet.org>
2018-07-23 11:00:27 -0500

    added networks

--- Poker-AI.py
+++ Poker-AI.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 from poker import client
+from poker.model import load
+import argparse
 import poker.tables as tables
 from poker.tables import *
 import sys
@@ -7,6 +9,7 @@ import poker.train
 
 SERVER='ws://poker-dev.wrs.club:3001'
 PLAYERNAME = 'hamburger'
+MODELNAME = 'basicPlayer'
 
 def start_ai():
     cli = client.Client(SERVER, PLAYERNAME)
@@ -22,9 +25,15 @@ if __name__ == "__main__":
         poker.train.train()
         exit()
 
-    if len(sys.argv) > 1:
-        PLAYERNAME = sys.argv[1]
+    parser = argparse.ArgumentParser(description='Launches AI poker player')
+    parser.add_argument('-m', '--model', help='Model name for neural net', required=False, defualt=MODELNAME) 
+    parser.add_argument('-p', '--player_name', help='Bot player name', required=False, default=PLAYERNAME) 
 
+    MODELNAME = args['model']
+    PLAYERNAME = args['player_name']
+
+    load(MODELNAME)
+    
     print(f"2 Players - K/K Unsuited - {win_chance(3, 'A', 'A', 'u')}")
     
     start_ai()
