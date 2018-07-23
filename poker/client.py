893d9809b07a3e2ec53c8cdda0647be68de59185
Coleman <powerofpickle@gmail.com>
2018-07-22 23:56:29 -0500

    integrated files from zoom

--- poker/client.py
+++ poker/client.py
@@ -2,12 +2,13 @@ import websockets
 from poker.ai import AI
 import asyncio
 import json
+import hashlib
 
 '''
 Handle server events using the AI member functions below. The 'event' parameter is
 the server-sent JSON already converted to a Python dictionary. The return value
 should be the client response as a Python dictionary that will be converted to JSON
-after it is return
+after it is returned
 '''
 
 class Client(object):
@@ -17,7 +18,9 @@ class Client(object):
 		return obj
 
 	def _new_peer(self, event):
-		print("New peer!")
+		for player_name in event['data']:
+			self.ai.get_player(player_name)
+
 		return None
 
 	def _start_reload(self, event):
@@ -52,10 +55,21 @@ class Client(object):
 		print(f"Event {event_name} went unhandled!")
 		return None
 
-	def __init__(self, playername):
+	def __init__(self, server, playername):
+		self.server = server
 		self.playername = playername
-		self.ai = AI()
-		print("Created player {}".format(playername))
+		md5 = hashlib.md5()
+		md5.update(playername.encode('utf8'))
+		md5 = md5.hexdigest()
+		self.ai = AI(md5)
+		print("Created player {} (MD5 {})".format(playername, md5))
+
+	def log(self, sender, msg):
+		log_msg = sender + ": " + msg + "\n"
+		print(log_msg)
+		with open(self.playername + "_log.txt", "a+") as log:
+			log.write(log_msg)
+
 
 	async def main_loop(self):
 		async with websockets.connect(self.server) as sock:
@@ -64,9 +78,12 @@ class Client(object):
 				client_response = self.on_event(json.loads(server_msg))
 
 				if client_response != None:
-					await sock.send(json.dumps(client_response))
+					client_response_txt = json.dumps(client_response)
+					self.log("Client", client_response_txt)
+					await sock.send(client_response_txt)
 
 				server_msg = await sock.recv()
+				self.log("Server", server_msg)
 
 	def run(self):
 		asyncio.get_event_loop().run_until_complete(self.main_loop())
---
+++ poker/odds.py
@@ -0,0 +1,54 @@
+import treys
+from treys import Card, Deck, Evaluator
+import time
+import random
+from random import shuffle as rshuffle
+
+random.seed(time.time())
+
+def odds(hand, board, num_players):
+    my_hand = [Card.new(hand[0]), Card.new(hand[1])]
+    remove_cards = [Card.new(hand[0]), Card.new(hand[1])]
+
+    my_board = []
+    
+    for i in range(len(board)):
+        my_board.append(Card.new(board[i]))
+        remove_cards.append(Card.new(board[i]))
+
+    my_deck = Deck()
+    
+    for i in range(len(remove_cards)):
+        my_deck.cards.remove(remove_cards[i])
+        
+    my_players = [my_hand]
+    evaluator = Evaluator()
+
+    count = 0;
+    for b in range(1000):
+        deck = Deck()
+        cards = my_deck.cards.copy()
+        rshuffle(cards)
+        deck.cards = cards
+
+        players = my_players.copy()
+        for j in range(num_players-1):
+            players.append(deck.draw(2))
+
+        board = my_board.copy()
+        while len(board) < 5:
+            board.append(deck.draw(1))
+
+        if evaluator.hand_summary(board, players) == 0:
+            count += 1
+        
+    return count/1000
+
+#-------------------------------------------------------------
+
+start = time.time()
+
+chance = odds(['As', 'Ad'], [], 10)
+end = time.time()
+print (end-start)
+print("Pre-Flop Ace: " + str(chance))
