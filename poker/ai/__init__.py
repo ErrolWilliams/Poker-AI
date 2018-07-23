1306c91f5cd06b88840e31ca98ec747257a39d7e
Coleman <powerofpickle@gmail.com>
2018-07-23 09:46:41 -0500

    added card helper function

--- poker/ai/__init__.py
+++ poker/ai/__init__.py
@@ -1,6 +1,7 @@
 from poker.table import Table
 from poker.player import Player
 import poker.ai.action
+from treys import Card
 
 class AI():
 
@@ -9,8 +10,15 @@ class AI():
 		self.table = Table()
 		self.player_id = player_id
 		self.player = self.table.get_player(self.player_id)
+	
+	def card_obj(self, card_str):
+		return Card.new(card_str[0] + card_str[1].lower())
 
 	def request_action(self):
+		cards = []
+		for card_str in self.table.board:
+			cards.append(self.card_obj(card_str))
+
 		return action.Call()
 
 	def request_bet(self):
