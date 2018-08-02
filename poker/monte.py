e3cd9315f4b6bd488d42336e4be001ebf31aef68
Coleman <powerofpickle@gmail.com>
2018-08-02 10:16:18 -0500

    httprint '

--- poker/monte.py
+++ poker/monte.py
@@ -9,7 +9,9 @@ def monteCarlo(board, hand, numPlayers, monteN):
     evaluator = Evaluator()
     playerHands = [None]*numPlayers
     winAmount = 0 
+    board_backup = board.copy()
     for time in range(int(monteN)):
+        board = board_backup.copy()
         monteDeck = [card for card in deck.cards if card not in board and card not in hand]
         for x in range(numPlayers):
             playerHands[x] = []
@@ -23,7 +25,7 @@ def monteCarlo(board, hand, numPlayers, monteN):
             del monteDeck[randomIndex]
         win = True
         handRank = evaluator.evaluate(board, hand)
-        for x in range(numPlayers)
+        for x in range(numPlayers):
             otherRank = evaluator.evaluate(board, playerHands[x])
             if otherRank < handRank:
                 win = False
