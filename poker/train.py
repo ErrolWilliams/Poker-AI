e95bafcddee67130d339a84521af91a327841df2
ErrolWilliams <errolw@US-ERROLW-E7470.us.trendnet.org>
2018-08-09 18:45:39 -0500

    removed prints

--- poker/train.py
+++ poker/train.py
@@ -155,7 +155,7 @@ def train(ai):
 		num_play    = random.choice([4,5,6,7,8,9])
 		play(num_rounds, ai, num_play)
 		ai.game_end()
-		if i % 20 == 0:      # every 100 games compete and save win percent
+		if i % 100 == 0:      # every 100 games compete and save win percent
 			score_rounds.append(float(i))
 			ai_scores.append(compete_stats(ai, num_play))
 			update_plot()
